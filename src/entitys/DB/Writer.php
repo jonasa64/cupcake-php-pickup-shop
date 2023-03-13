066622e442056c7160d58ac8b243559b5b24bf95
jonasa64 <hvidovrefifa@gmail.com>
2023-03-13 06:34:58 +0100

    Functions to DB writer

--- src/entitys/DB/Writer.php
+++ src/entitys/DB/Writer.php
@@ -2,4 +2,14 @@
 
 class Writer
 {
+    public function insert($tableName, $data)
+    {
+    }
+    public function update($tableName, $data, $whereSql, $whereVales)
+    {
+    }
+
+    public function delete($tableName, $whereSql, $whereVales)
+    {
+    }
 }
