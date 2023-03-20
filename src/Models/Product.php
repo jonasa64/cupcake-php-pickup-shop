a6d94f84932da8943757faa097b5dbe360d46fef
jonasa64 <hvidovrefifa@gmail.com>
2023-03-20 06:05:49 +0100

    Added setters

--- src/Models/Product.php
+++ src/Models/Product.php
@@ -11,11 +11,15 @@ class Product
     private int $quantityInStock;
     private string $toppingFlavor;
     private string $bottomFlavor;
-    private string $tableName = "products";
 
     public function getId()
     {
         return $this->id;
+    }
+
+    public function setId(int $id)
+    {
+        $this->id = $id;
     }
 
     public function getName()
@@ -23,9 +27,19 @@ class Product
         return $this->name;
     }
 
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
     public function getDescription()
     {
         return $this->description;
+    }
+
+    public function setDescription(string $description)
+    {
+        $this->description = $description;
     }
 
     public function getPrice()
@@ -33,9 +47,19 @@ class Product
         return $this->price;
     }
 
+    public function setPrice(float $price)
+    {
+        $this->price = $price;
+    }
+
     public function getImageSrc()
     {
         return $this->imageSrc;
+    }
+
+    public function setImageSrc(string $imageSrc)
+    {
+        $this->imageSrc = $imageSrc;
     }
 
     public function getQuantityInStock()
@@ -43,13 +67,28 @@ class Product
         return $this->quantityInStock;
     }
 
+    public function setQuantityInStock(int $quantityInStock)
+    {
+        return $this->quantityInStock = $quantityInStock;
+    }
+
     public function getToppingFlavor()
     {
         return $this->toppingFlavor;
     }
 
+    public function setToppingFlavor(string $toppingFlavor)
+    {
+        $this->toppingFlavor = $toppingFlavor;
+    }
+
     public function getBottomFlavor()
     {
         return $this->bottomFlavor;
+    }
+
+    public function setBottomFlavor(string $bottomFlavor)
+    {
+        $this->bottomFlavor = $bottomFlavor;
     }
 }
