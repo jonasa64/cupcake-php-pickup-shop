34a2a47cf819134c2398e2818328f574548da0d4
jonasa64 <hvidovrefifa@gmail.com>
2023-03-22 05:40:12 +0100

    Base controller

---
+++ src/Controllers/Controller.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Cupcake\Controllers;
+
+class Controller
+{
+
+
+    public function __construct()
+    {
+    }
+
+    protected function renderView($name, $data = [])
+    {
+    }
+}
