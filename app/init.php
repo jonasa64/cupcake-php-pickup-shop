eb62bd5aec621a9e915d9ef0ee5c12c1fa161f0f
jonasa64 <hvidovrefifa@gmail.com>
2023-04-02 00:53:14 +0200

    APP_ROOT const

--- app/init.php
+++ app/init.php
@@ -1,5 +1,7 @@
 <?php
 
-require_once "../src/Controllers/Core.php";
+define("APP_ROOT", $_SERVER["DOCUMENT_ROOT"] . "/cupcake-php-pickup-shop/");
+
+require_once APP_ROOT .  "src/Controllers/Core.php";
 
 $init = new Cupcake\Controllers\Core;
