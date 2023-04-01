16674bf8dee5914a26148fe7321565347db6cfbe
jonasa64 <hvidovrefifa@gmail.com>
2023-04-02 00:52:59 +0200

    Products views

---
+++ src/Views/Products/index.php
@@ -0,0 +1,18 @@
+<div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+    <?php foreach ($data as $product) : ?>
+        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+                <img class="rounded-t-lg" src="<?= $product->getImageSrc() ?>" />
+            </a>
+            <div class="p-5">
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><?= $product->getName() ?></h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"><?= $product->getPrice() ?></p>
+                <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Read more
+                </a>
+            </div>
+        </div>
+    <?php endforeach; ?>
+</div>
